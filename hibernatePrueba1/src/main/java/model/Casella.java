64eb748742d7b0c66b4c9f03d37be90b3670d0c9
Kiiing8r <mariogago22@gmail.com>
2024-02-01 15:56:29 +0100

    Act

--- hibernatePrueba1/src/main/java/model/Casella.java
+++ hibernatePrueba1/src/main/java/model/Casella.java
@@ -1,5 +1,39 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Casella")
 public class Casella {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_CASELLA")
+	private int id_Casella;
+	
+	@Column(name = "TIPUS_CASELLA", nullable = false)
+	private String tipusCasella;
+	
+	@Column(name = "POSICIO", nullable = false)
+	private int posicio;
+	
+	@ManyToOne
+	@JoinColumn(name="ID_PARTIDA")
+	private int id_Partida;
+
+	public Casella(int id_Casella, String tipusCasella, int posicio, int id_Partida) {
+		super();
+		this.id_Casella = id_Casella;
+		this.tipusCasella = tipusCasella;
+		this.posicio = posicio;
+		this.id_Partida = id_Partida;
+	}
+	
 }
--- hibernatePrueba1/src/main/java/model/Fitxa.java
+++ hibernatePrueba1/src/main/java/model/Fitxa.java
@@ -1,5 +1,39 @@
 package model;
 
-public class Fitxa {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Fitxa")
+public class Fitxa {
+	
+	@Column(name="POSICIÓ", nullable=false)
+	private int posicio;
+	
+	@Column(name="ACTIVA", nullable=false)
+	private boolean isActive;
+	
+	@ManyToOne
+	@JoinColumn(name="ID_JUGADOR")
+	private int id_Jugador;
+	
+	@ManyToOne
+	@JoinColumn(name="ID_PARTIDA")
+	private int id_Partida;
+	
+	private int passos;
+
+	public Fitxa(int id_Jugador, int id_Partida) {
+		super();
+		this.passos = 0;
+		this.posicio = 0;
+		this.isActive = false;
+		this.id_Jugador = id_Jugador;
+		this.id_Partida = id_Partida;
+	}
+	
+	
 }
